b2f19226b49e174cc9986f7b8da69af7b21d7574
Alexandru Bosii <Alexandru.Bosii@endava.com>
2017-05-29 16:15:27 +0300

    strange things

--- src/main/java/com/eter/spark/app/inventoryanalysis/transformer/NormalizeScoreTransformer.java
+++ src/main/java/com/eter/spark/app/inventoryanalysis/transformer/NormalizeScoreTransformer.java
@@ -22,7 +22,9 @@ public class NormalizeScoreTransformer extends Transformer {
     @Override
     public Dataset<Row> transform(Dataset<?> dataset) {
         Dataset<Row> normal = dataset.groupBy(dataset.col(scoreCol))
-                .agg(dataset.col(scoreCol), max(dataset.col(scoreCol)))
+                .agg(dataset.col(scoreCol), max(dataset.col(scoreCol)));
+
+        normal = normal
                 .withColumn(outputCol, dataset.col("max(" + scoreCol + ")").minus(dataset.col(scoreCol)));
 
         return normal;
